65d88ca260a72b82a6c2460cdd1a7d47b481fd4b
Debojit Bhattacharjee <debojit.bhattacharjee@ibm.com>
2026-01-21 02:27:30 +0530

    Naming convenion changed for extent report

--- src/main/java/com/ibm/framework/reports/ExtentReport.java
+++ src/main/java/com/ibm/framework/reports/ExtentReport.java
@@ -1,8 +1,5 @@
 package com.ibm.framework.reports;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
@@ -17,8 +14,8 @@ public class ExtentReport {
 		{
 			return;
 		}
-		String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-		String reportPath = System.getProperty("user.dir") + "/target/extent-report/ExtentReport_" + timestamp + ".html";
+		String reportPath = System.getProperty("user.dir")
+		        + "/target/extent-report/ExtentReport.html";
 		
         ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
         reporter.config().setReportName("TDDBank Automation Report");
